2887e878bb53b2bfe4f161bfa32b18e0bba37fb1
CodeRed <antonio.martinez1653204@gmail.com>
2019-03-30 18:12:01 -0500

    added a reset function

--- assets/javascript/crystal.js
+++ assets/javascript/crystal.js
@@ -7,3 +7,28 @@ $( document ).ready(function(){
         $('#randomNumber').text(Random);
         // Appending random number to the randomNumber id in the html doc
         //
+    var num1= Math.floor(Math.random()*11+1)
+    var num2= Math.floor(Math.random()*11+1)
+    var num3= Math.floor(Math.random()*11+1)
+    var num4= Math.floor(Math.random()*11+1)
+    // Setting up random numbers for each evolution stone
+    // Random number has to be between 1 - 12
+    var userTotal= 0; 
+    var wins= 0;
+    var losses = 0;
+    //  Decaring variables for wins and loses
+  $('#numberWins').text(wins);
+  $('#numberLosses').text(losses);
+  
+  //resets the game
+  function reset(){
+    Random=Math.floor(Math.random()*101+19);
+    console.log(Random)
+    $('#randomNumber').text(Random);
+    num1= Math.floor(Math.random()*11+1);
+    num2= Math.floor(Math.random()*11+1);
+    num3= Math.floor(Math.random()*11+1);
+    num4= Math.floor(Math.random()*11+1);
+    userTotal= 0;
+    $('#finalTotal').text(userTotal);
+    } 
